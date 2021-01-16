95a826e6c170dcf05086284ef062a526da2d5f6f
Ed Timmer <edtimmer@gmail.com>
2021-01-15 20:41:32 -0500

    finished project

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,10 +1,18 @@
 const path = require('path')
+const { ipcRenderer } = require('electron')
 const osu = require('node-os-utils')
 const cpu = osu.cpu
 const mem = osu.mem
 const os = osu.os
 
-let cpuOverload = 80
+let cpuOverload
+let alertFrequency
+
+// Get settings & values
+ipcRenderer.on('settings:get', (e, settings) => {
+  cpuOverload = +settings.cpuOverload
+  alertFrequency = +settings.alertFrequency
+})
 
 // Run every 2 seconds
 setInterval(() => {
@@ -20,6 +28,18 @@ setInterval(() => {
     } else {
       document.getElementById('cpu-progress').style.background = '#30c88b';
     }
+
+    // Check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: 'CPU Overload',
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, 'img', 'icon.png'),
+      })
+
+      localStorage.setItem('lastNotify', +new Date())
+    }
+
   })
 
   // CPU Free
@@ -54,4 +74,28 @@ function secondsToDhms(seconds) {
   const m = Math.floor((seconds % 3600) / 60)
   const s = Math.floor(seconds % 60)
   return `${d}d, ${h}h, ${m}m, ${s}s`
+}
+
+// Send notification
+function notifyUser(options) {
+  new Notification(options.title, options)
+}
+
+// Check how much time has passed since notification
+function runNotify(frequency) {
+  if (localStorage.getItem('lastNotify') === null) {
+    // Store timestamp
+    localStorage.setItem('lastNotify', +new Date())
+    return true
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')))
+  const now = new Date()
+  const diffTime = Math.abs(now - notifyTime)
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60))
+
+  if(minutesPassed > frequency) {
+    return true
+  } else {
+    return false
+  }
 }
